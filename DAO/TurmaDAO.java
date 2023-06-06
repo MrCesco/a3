5000bc922bb7fd63fdfe10620a9dccb7a6b97273
MrCesco <109219368+MrCesco@users.noreply.github.com>
2023-06-06 15:46:34 -0300

    upando projeto A3

---
+++ DAO/TurmaDAO.java
@@ -0,0 +1,49 @@
+package DAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import model.Turma;
+
+public class TurmaDAO {
+    
+
+    public static List<Turma> getTurmas() {
+        Connection conn = DBConnection.getInstance().getConnection();
+        List<Turma> retorno = new ArrayList<>();
+        try {
+            PreparedStatement ps = conn.prepareStatement("select * from turmas");
+            ResultSet resultSet = ps.executeQuery();
+            //resultSet.first();
+            while (resultSet.next()) {
+                Turma tmp = new Turma(resultSet.getInt("idTurmas"), resultSet.getString("apelido"),
+                resultSet.getInt("capacidade"), resultSet.getInt("ucs_iducs"));
+                retorno.add(tmp);
+                
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return retorno;
+    }
+
+    public static Turma salvarTurma(Turma turma) {
+        Connection conn = DBConnection.getInstance().getConnection();
+        try {
+            PreparedStatement ps = conn.prepareStatement("insert into turmas (apelido, capacidade, ucs_iducs) values (?,?,?)");
+            ps.setString(1, turma.getApelido());
+            ps.setInt(2, turma.getCapacidade());
+            ps.setInt(3, turma.getUcs_iducs());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return null;
+    }
+    
+}
---
+++ view/AppView.java
@@ -0,0 +1,16 @@
+package view;
+import java.util.Scanner;
+
+public class AppView {
+    public static int menuInicial() {
+        int retorno = -1;
+        System.out.println("1. Listar turmas");
+        System.out.println("2. Incluir turma");
+        System.out.println("");
+        System.out.println("Escolha sua opção");
+        Scanner scan = new Scanner(System.in);
+        retorno = scan.nextInt();
+
+        return retorno;
+    }
+}
