672e540a9b6800ca27c8cee17044bfc87dbae4ed
Scarlet <lscarletproductions7@gmail.com>
2023-06-15 10:34:04 -0300

    alterações nos construtores

--- models/Abrigo.java
+++ models/Abrigo.java
@@ -4,16 +4,31 @@ public class Abrigo extends Pessoa {
     private String cnpj;
     private String r_social;
 
-    public Abrigo(int id, String nome, String endereco, String cpf, String email, String telefone, String tipo_cadastro,
-            String cnpj, String r_social) {
-        super(id, nome, endereco, cpf, email, telefone, tipo_cadastro);
+    public Abrigo(String nome, String endereco, String cpf, String email, String telefone, String cnpj,
+            String r_social) {
+        super(nome, endereco, cpf, email, telefone);
         this.cnpj = cnpj;
+        this.r_social = r_social;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getR_social() {
+        return r_social;
+    }
+
+    public void setR_social(String r_social) {
         this.r_social = r_social;
     }
 
     @Override
     public String toString() {
-        return super.toString() + "CNPJ: " + cnpj + "\n" + "Razão Social: " + r_social + "\n";
+        return "Abrigo [cnpj=" + cnpj + ", r_social=" + r_social + "]";
     }
-
 }
--- models/Adotante.java
+++ models/Adotante.java
@@ -1,24 +1,24 @@
 package models;
 
 public class Adotante extends Pessoa {
-    private int credencial_adotante;
-    
-    public Adotante(int id, String nome, String endereco, String cpf, String email, String telefone,
-            String tipo_cadastro, int credencial_adotante) {
-        super(id, nome, endereco, cpf, email, telefone, tipo_cadastro);
-        this.credencial_adotante = credencial_adotante;
+    private String tipo_cadastro;
+
+    public Adotante(String nome, String endereco, String cpf, String email, String telefone,
+            String tipo_cadastro) {
+        super(nome, endereco, cpf, email, telefone);
+        this.tipo_cadastro = tipo_cadastro;
     }
 
-    public int getCredencial_adotante() {
-        return credencial_adotante;
+    public String getTipo_cadastro() {
+        return tipo_cadastro;
     }
 
-    public void setCredencial_adotante(int credencial_adotante) {
-        this.credencial_adotante = credencial_adotante;
+    public void setTipo_cadastro(String tipo_cadastro) {
+        this.tipo_cadastro = tipo_cadastro;
     }
 
     @Override
     public String toString() {
-        return super.toString() + "Crendencial: " + credencial_adotante + "\n";
+        return "Adotante [tipo_cadastro=" + tipo_cadastro + "]";
     }
 }
